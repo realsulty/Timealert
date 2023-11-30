14885d0f5366e99d57151823f702ecb2d3f8d2cc
realsulty <Sultanobm1@gmail.com>
2023-11-30 13:27:03 +0000

    adjusted js files

---
+++ files/script.js
@@ -0,0 +1,36 @@
+const d = new Date(2023, 5, 10, 15, 0, 0);
+const month = d.getMonth();
+const hour = d.getHours();
+
+
+switch (month) {
+    case 1:
+        console.log('Its January');
+        break;
+        
+    case 2:
+        console.log('Its Febuary');
+        break;
+    case 3:
+        console.log('Its March');
+        break;
+        default:
+            console.log('It is just another Normal Day!')
+}
+
+
+
+switch (true) {
+    case hour < 12:
+    console.log('Good morning you lucky charm');
+    break;
+ 
+    case hour < 18:
+    console.log('Its Gym time buddy, move dat Ass');
+    break;
+ 
+    case hour === 22:
+    console.log('Better be on you bed by now!');
+    break;
+ 
+}
