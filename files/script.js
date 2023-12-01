8171d222b9db0c619843ee8976673bcbe3748ba9
realsulty <Sultanobm1@gmail.com>
2023-12-01 23:19:59 +0000

    new ternary operatprs

--- files/script.js
+++ files/script.js
@@ -36,4 +36,30 @@ switch (true) {
 }
 
 
-// Ternary operator will be used very ofter
+// // Ternary operator will be used very ofter
+
+// const age = 2;
+
+// age >= 18 ? console.log('you can vote!') : console.log('You can not vote!')
+
+// // Assigning a condintioanl value to a variable 
+// const canVote = age >= 18 ? true : false;
+// const canVote2 = age >= 18 ? 'You can vote' : 'You can not Vote!';
+
+// console.log(canVote);
+// console.log(canVote2);
+
+const auth = false;
+let redirect
+// will use the longer method first 
+if (auth) {
+
+    alert('Welocome to the dashboard');
+    redirect = '/dashboard';
+ } else {
+    alert('Access Denied');
+    redirect = '/login';
+
+ }
+
+console.log(redirect);
